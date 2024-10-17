db1c5eead839a4c4705cf8f7811fe8542805b5c3
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-17 18:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstChar(text) {
-  // your code here
+  // Trim whitespace from both end
+  const trimmedStr = text.trim();
+  
+  // Return the first character or an empty string if trimmedStr is empty
+  return trimmedStr[0] || '';
 }
 
 // Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+//Uncomment the following line to show the prompt popu
+const text = prompt("Enter text:");
 alert(firstChar(text));
